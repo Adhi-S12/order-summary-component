c11f286cb4e66ebe57410b4d8ce4f831fc2d4c71
Adhi <adhipathipandiyan.s12@gmail.com>
2022-03-04 01:57:03 +0530

    Completed the challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './images/illustration-hero.svg';
+import MusicIcon from './images/icon-music.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className="App">
+			<div className="order-summary-component">
+				<div className="order-summary-component-image">
+					<img src={Hero} alt="illustration hero" />
+				</div>
+				<div className="order-summary-details">
+					<h2 className="osd-title">Order Summary</h2>
+					<p className="osd-desc">
+						You can listen to million of songs, audiobooks, and podcasts on any device anywhere you like!
+					</p>
+					<div className="osd-plan-change">
+						<div className="osd-plan-change-left">
+							<img src={MusicIcon} alt="Music Icon" />
+							<div className="osd-plan-change-plans">
+								{/* <span className="osd-plan-change-plans-title">Annual Plan</span> <br />
+								<span className="osd-plan-change-plans-price">$55.99/year</span> */}
+								<h3 className="osd-plan-change-plans-title">Annual Plan</h3>
+								<h6 className="osd-plan-change-plans-price">$55.99/year</h6>
+							</div>
+						</div>
+						<div className="osd-plan-change-right">
+							<a href="/">Change</a>
+						</div>
+					</div>
+
+					<div className="osd-payment-button">Proceed to Payment</div>
+					<div className="osd-cancel-button">Cancel Order</div>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default App;
